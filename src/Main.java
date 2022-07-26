60265c2ead13198c383d5eb853fef0240f6f3c0c
chmutenkoa <alenachm@gmail.com>
2022-07-26 16:26:51 +0300

    trimAverage added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = { 2,5,7,9,18,0 };
+        int[] sales = { 2, 5, 7, 9, 18, 10, 0 };
         SalesManager manager = new SalesManager(sales);
         System.out.println(manager.max());
+        int averSale = manager.trimAverage();
+        System.out.println(averSale);
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,21 @@ public class SalesManager {
         }
         return max;
     }
+    public int trimAverage() {
+        int max = 0;
+        int min = sales[0];
+        int sum = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] <= min) {
+                sum += min;
+                min = sales[i];
+            } else if (sales[i] >= max) {
+                sum += max;
+                max = sales[i];
+            } else {
+                sum += sales[i];
+            }
+        }
+        return sum / (sales.length - 2);
+    }
 }
